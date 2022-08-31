2d8107d43e2f1eb3fd360fe72c0d984c93a34e02
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-08-31 19:42:52 +0600

    added create, delete and update contact controller

--- src/api/contact/controllers/contact.js
+++ src/api/contact/controllers/contact.js
@@ -9,39 +9,56 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::contact.contact', ({strapi}) => {
     return {
         async delete(ctx) {
-
             try {
 
-                            // console.log(ctx, 'ctx');
-            // console.log(ctx.state, 'state');
-            // console.log(ctx.state.user, 'user');
-            // console.log(ctx.params, 'params');
-            // console.log(ctx.request.body, 'body');
+                const {id} = ctx.params;
+                const {user} = ctx.state;
 
-            const {id} = ctx.params;
-            const {user} = ctx.state;
+                const contact = await strapi.service('api::contact.contact').findOne(+id, {
+                    populate: 'author'
+                });
 
-            ctx.query = { ...ctx.query, populate: 'author' }
-            const {data} = await super.findOne(ctx);
-            const {id: contactAuthorId} = data?.attributes?.author?.data;
-            
-            const contact = await strapi.entityService.findOne('api::contact.contact', +id, {
-                populate: 'author'
-            });
+                if(!contact) return ctx.notFound('Contact is not found to be deleted');
+                if(contact.author.id !== user.id) return ctx.unauthorized('You are not the owner of the contact');
 
-            // const contact = await strapi.service('api::contact.contact').findOne(+id, {
-            //     populate: 'author'
-            // });
+                const response = await super.delete(ctx);
+                return response;
+                    
+            } catch (error) {
+                ctx.internalServerError('Unknown error');
+            }
+        },
+        async create(ctx) {
+            try {
 
-            console.log(contact, 'contact');
+                const {user} = ctx.state;
+                ctx.request.body.data.author = user.id;
 
-
-            // const response = await super.delete(ctx);
-            // return response;
+                const response = await super.create(ctx);
+                return response;
                 
             } catch (error) {
-                
-                console.log(error, 'error');
+                ctx.internalServerError('Unknown error');
+            }
+        },
+        async update(ctx) {
+            try {
+
+                const {id} = ctx.params;
+                const {user} = ctx.state;
+
+                const contact = await strapi.service('api::contact.contact').findOne(+id, {
+                    populate: 'author'
+                });
+
+                if(!contact) return ctx.notFound('Contact is not found to be update');
+                if(contact.author.id !== user.id) return ctx.unauthorized('You are not the owner of the contact to update');
+
+                const response = await super.update(ctx);
+                return response;
+
+            } catch (error) {
+                ctx.internalServerError('Unknown error');
             }
         }
     }
