2b083e54a57450f6ac657f8d96c1603a0fd0141e
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-09-14 22:10:13 +0600

    added custom controller in profile

--- src/api/profile/controllers/profile.js
+++ src/api/profile/controllers/profile.js
@@ -1,9 +1,39 @@
-'use strict';
+"use strict";
 
 /**
  * profile controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::profile.profile');
+module.exports = createCoreController("api::profile.profile", ({ strapi }) => {
+  return {
+    async create(ctx) {
+      try {
+        const { user } = ctx.state;
+        const files = ctx.request.files;
+
+        const parsedData = JSON.parse(ctx?.request?.body?.data);
+        parsedData.user = user.id;
+
+        const response = await strapi
+          .service("api::profile.profile")
+          .create({ data: parsedData, files, populate: "*" });
+
+        // const userResponse = await strapi.query("plugin::users-permissions.user").update({
+        //   where: { id: user.id },
+        //   data: {
+        //     isProfile: true,
+        //   },
+        //   populate: true,
+        // });
+        // console.log(userResponse, 'userResponse');
+
+        return response;
+      } catch (error) {
+        console.log(error, "error");
+        ctx.internalServerError("Internal Server Error");
+      }
+    },
+  };
+});
