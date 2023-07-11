68ffd305303611ac21b14f5d4733694ccef41ec8
Kawsar <web.kawsarahmed@gmail.com>
2023-07-11 18:54:27 +0600

    added production config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,15 +1,10 @@
 const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => {
-    const url = parse(env("PRODUCTION_URL"));
-    console.log(url, 'url')
-
-    return {
-        host: env('PRODUCTION_HOST'),
-        port: env.int('PORT'),
-        app: {
-          keys: env.array('APP_KEYS'),
-        },
-      }
-};
+module.exports = ({ env }) => ({
+  host: env('PRODUCTION_HOST'),
+  port: env.int('PORT'),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+})
   
