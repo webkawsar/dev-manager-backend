dc3e85f6fd77915d1ad8e03ae97d7323facb54f3
Kawsar <web.kawsarahmed@gmail.com>
2023-07-11 19:59:49 +0600

    added config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
+    host: env('HOST'),
+    port: env.int('PORT'),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
     url: env('PUBLIC_URL')
-});
+  });
   
